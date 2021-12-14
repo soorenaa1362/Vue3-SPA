492d1aa21d5c266e66d6c4de73e1e41bb3fd3310
Hafez <h@pardislab.local>
2021-12-14 16:35:45 +0330

    02 Header Ok and Before Install Axios

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
-    <h1>ZeroWave.ir</h1>
-
-    <div class="container">
-        <div class="alert alert-primary" role="alert">
-            A simple primary alert—check it out!
-        </div>
-    </div>
+    <Header />
 
     <router-view></router-view>
 </template>
 
 <script>
+import Header from './components/sections/Header.vue'
 
 export default {
-    name: 'App'
+    name: 'App',
+    components: {
+        Header
+    }
 }
 </script>
 
---
+++ src/pages/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-6">
+                <p>
+                    Laravel makes API authentication a breeze using Laravel Passport, 
+                    which provides a full OAuth2 server implementation for your Laravel 
+                    application in a matter of minutes. Passport is built on top of the 
+                    League OAuth2 server that is maintained by Andy Millington and Simon Hamp.
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
