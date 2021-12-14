5734942c898cd77e9ef752c3797778f04c9e368d
Hafez <h@pardislab.local>
2021-12-14 16:50:45 +0330

    02 Add Axios Library to Project and Index User Ok

---
+++ src/pages/users/Index.vue
@@ -0,0 +1,29 @@
+<template>
+    <h1>Users</h1>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    setup(){
+        function getUsers(){
+            axios
+            .get('https://jsonplaceholder.typicode.com/users')
+            .then(function (response) {
+                // handle success
+                console.log(response.data);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            });
+        }
+
+        getUsers();
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 import {createRouter, createWebHistory} from 'vue-router';
 import Home from './pages/Home.vue';
+import IndexUser from './pages/users/Index.vue'
 
 const routes = [
-    { path: "/", name: "home", component:Home }
+    { path: "/", name: "home", component: Home },
+    { path: "/users", name: "users", component: IndexUser }
 ];
 
 const router = createRouter({
